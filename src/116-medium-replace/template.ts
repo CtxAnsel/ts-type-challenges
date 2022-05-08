5d62be92c030b05a624b0d822dc1c5cf1cbc10a9
chentianxi <chentianxi1212@163.com>
2022-05-08 13:40:16 +0800

    feat: replace

--- src/116-medium-replace/template.ts
+++ src/116-medium-replace/template.ts
@@ -1,1 +1,9 @@
-type Replace<S extends string, From extends string, To extends string> = S extends
+type Replace<
+  S extends string,
+  From extends string,
+  To extends string
+> = S extends `${infer front}${From}${infer last}`
+  ? From extends ""
+    ? S
+    : `${front}${To}${last}`
+  : S;
