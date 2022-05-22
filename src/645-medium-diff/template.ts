2c0ca8b55384dce4438a9ddbd04da86407a53b1d
chentianxi <chentianxi1212@163.com>
2022-05-22 12:33:38 +0800

    feat: diff的第二种实现

--- src/645-medium-diff/template.ts
+++ src/645-medium-diff/template.ts
@@ -4,10 +4,15 @@ type TT<K1, K2> = K1 extends K2
   ? never
   : K1;
 
-type Diff<O extends object, O1 extends object> = {
+type Diff2<O extends object, O1 extends object> = {
   [K in TT<keyof (O & O1), keyof (O | O1)>]: K extends keyof O
     ? O[K]
     : K extends keyof O1
     ? O1[K]
     : never;
 };
+
+type Diff<O, O1> = {
+  [P in (Exclude<keyof O, keyof O1> | Exclude<keyof O1, keyof O>)]:
+  P extends keyof O ? O[P] : P extends keyof O1 ? O1[P] : never
+}
