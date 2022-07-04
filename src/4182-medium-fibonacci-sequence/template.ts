3baccad24e210fa477a76355da212dd8286df6ca
chentianxi <chentianxi1212@163.com>
2022-07-04 23:54:21 +0800

    feat: add fibonacci

---
+++ src/4182-medium-fibonacci-sequence/template.ts
@@ -0,0 +1,13 @@
+type Fibonacci<
+  T extends number,
+  N1 extends number[] = [],
+  N2 extends number[] = [number],
+  V extends number[] = [number]
+> = T extends 0
+  ? never
+  : T extends V["length"]
+  ? N2["length"]
+  : Fibonacci<T, N2, [...N1, ...N2], [...V, number]>;
+
+type result1 = Fibonacci<3>;
+type result2 = Fibonacci<8>;
