5fd0b9c37f56219088091d1649cd9ac968908d03
chentianxi <chentianxi1212@163.com>
2022-07-18 09:29:33 +0800

    feat: add chunk

---
+++ src/4499-medium-chunk/template.ts
@@ -0,0 +1,11 @@
+type Chunk<
+  T extends any[],
+  Step extends number,
+  L extends any[] = []
+> = T extends [infer Head, ...infer Rest]
+  ? L["length"] extends Step
+    ? [L, ...Chunk<T, Step, []>]
+    : [...Chunk<Rest, Step, [...L, Head]>]
+  : L extends []
+  ? []
+  : [L];
